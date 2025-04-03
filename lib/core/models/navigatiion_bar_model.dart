5bf6187460e52e3f0fbe77c4f73ce419b36fb40b
abdullah Awad <125440265+AAELBDAWY1318@users.noreply.github.com>
2025-04-03 03:05:44 -0700

    create the base of structure

---
+++ lib/core/models/navigatiion_bar_model.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SampleListModel {
+  Widget? leading;
+  String? title;
+  String? subTitle;
+  Widget? trailing;
+  IconData? icon;
+  IconData? alternateIcon;
+  Function? onTap;
+  Color? colors;
+  Widget? launchWidget;
+  Widget? screen;
+
+  SampleListModel(
+      {this.leading,
+      this.title,
+      this.subTitle,
+      this.colors,
+      this.icon,
+      this.alternateIcon,
+      this.trailing,
+      this.onTap,
+      this.screen,
+      this.launchWidget});
+}
---
+++ lib/features/guess/presentation/view/guess_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class GuessView extends StatelessWidget {
+  const GuessView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text("Guess"),
+    );
+  }
+}
