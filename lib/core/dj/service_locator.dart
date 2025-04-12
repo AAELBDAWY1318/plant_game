4830b37521495f8f253034a4dabd69cbef3c779d
abdullah Awad <125440265+AAELBDAWY1318@users.noreply.github.com>
2025-04-11 20:26:34 -0700

    add needed independence to service locator and write logic of scan plant cubit

--- lib/core/dj/service_locator.dart
+++ lib/core/dj/service_locator.dart
@@ -1,6 +1,13 @@
+import 'package:data_connection_checker_tv/data_connection_checker.dart';
+import 'package:dio/dio.dart';
 import 'package:get_it/get_it.dart';
+import 'package:plant_game/core/network_info/network_info.dart';
 import 'package:plant_game/core/utils/size_config.dart';
+import 'package:plant_game/features/home/data/repository/scan_plants_repository.dart';
+import 'package:plant_game/features/home/data/service/scan_plants_remote_service.dart';
 
+import '../database/api/api_conscumer.dart';
+import '../database/api/dio_consumer.dart';
 import '../utils/navigation_service.dart';
 
 final GetIt sl = GetIt.instance;
@@ -8,4 +15,17 @@ final GetIt sl = GetIt.instance;
 void setupServiceLocator() {
   sl.registerLazySingleton<NavigationService>(() => NavigationService());
   sl.registerLazySingleton<SizeConfig>(() => SizeConfig());
+
+  sl.registerLazySingleton<Dio>(() => Dio());
+  sl.registerLazySingleton<ApiConsumer>(() => DioConsumer(dio: sl<Dio>()));
+  sl.registerLazySingleton<NetworkInfo>(
+      () => NetworkInfoImpl(dataConnectionChecker: DataConnectionChecker()));
+
+  sl.registerLazySingleton<ScanPlantsRemoteService>(
+      () => ScanPlantsRemoteService(apiConsumer: sl<ApiConsumer>()));
+
+  sl.registerLazySingleton<ScanPlantsRepository>(() => ScanPlantsRepository(
+        scanPlantsRemoteService: sl<ScanPlantsRemoteService>(),
+        networkInfo: sl<NetworkInfo>(),
+      ));
 }
--- lib/features/home/presentation/cubit/cubit.dart
+++ lib/features/home/presentation/cubit/cubit.dart
@@ -1,6 +1,21 @@
+import 'dart:io';
+
 import 'package:bloc/bloc.dart';
+import 'package:plant_game/features/home/data/repository/scan_plants_repository.dart';
 import 'package:plant_game/features/home/presentation/cubit/state.dart';
+
+import '../../../../core/dj/service_locator.dart';
 
 class ScanPlantCubit extends Cubit<ScanPlatState> {
   ScanPlantCubit() : super(InitialState());
+
+  getPlantInfo(File imageFile) async {
+    emit(ScanImageLoading());
+    var res = await sl<ScanPlantsRepository>().getPlantInfo(imageFile);
+    res.fold((errorMessage) {
+      emit(ScanImageFailure(errorMessage: errorMessage));
+    }, (plantModel) {
+      emit(ScanImageSuccess(plantModel: plantModel));
+    });
+  }
 }
